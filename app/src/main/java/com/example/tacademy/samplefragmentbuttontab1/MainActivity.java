aa6efef88aa018b327931527950e41571df11b31
kiss9815 <kiss9815@gmail.com>
2016-01-20 16:56:50 +0900

    탭 같은거 눌려도 변하지 않게 하고 버튼 셀렉터 추가

--- app/src/main/java/com/example/tacademy/samplefragmentbuttontab1/MainActivity.java
+++ app/src/main/java/com/example/tacademy/samplefragmentbuttontab1/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button selectButton;
 
     Fragment fragment;
     private static final String TAB1_TAG = "tab1";
@@ -19,37 +20,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button)findViewById(R.id.btn_tab1);
+        Button btn = (Button) findViewById(R.id.btn_tab1);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Fragment f = new Tab1Fragment();
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                ft.replace(R.id.container, f, TAB1_TAG); //이코드로 탭1, 탭2 구분
-                ft.commit();
+                Fragment old = getSupportFragmentManager().findFragmentByTag(TAB1_TAG);  // 이미 생성되어 있으면 태그를 읽어서 다시 생성하지 않도록한다.
+                if(old == null) {
+                    Fragment f = new Tab1Fragment();
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.container, f, TAB1_TAG); //이코드로 탭1, 탭2 구분
+                    ft.commit();
+                    setSelectButton((Button) v);
+                }
             }
         });
 
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             Fragment f = new Tab1Fragment();
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.container, f, TAB1_TAG);
             ft.commit();
+            setSelectButton(btn); //?
         } //처음에 액티비티 띄울때 나오는 탭 , 중간에 끄고 다시시작해서 savedInstanceState가 있으면 실행하지 않음
 
-         btn = (Button)findViewById(R.id.btn_tab2);
+        btn = (Button) findViewById(R.id.btn_tab2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Fragment f = new Tab2Fragment();
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                ft.replace(R.id.container, f, TAB2_TAG);
-                ft.commit();
+                Fragment old = getSupportFragmentManager().findFragmentByTag(TAB2_TAG);  // 이미 생성되어 있으면 태그를 읽어서 다시 생성하지 않도록한다.
+                if(old == null) {
+                    Fragment f = new Tab2Fragment();
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.container, f, TAB2_TAG);
+                    ft.commit();
+                    setSelectButton((Button) v);
+                }
             }
         });
-
-
     }
+
+        private void setSelectButton(Button button){ //버튼 셀렉트 하면 바뀌가 하는 코드
+        if(selectButton != button){
+            if(selectButton != null){
+                selectButton.setSelected(false);
+            }
+            selectButton = button;
+            selectButton.setSelected(true);
+        }
+    }
+
+
 }
